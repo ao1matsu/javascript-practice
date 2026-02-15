8d4cfffa14f154bcfb3a3af56aa2bb4cd9a1450d
ao1matsu <ao1.matsu08@gmail.com>
2026-02-15 22:02:00 +0900

    Update script.js

--- random-game/script.js
+++ random-game/script.js
@@ -1,11 +1,11 @@
-function PlayGame() {
-const randomNumber = Math.floor(Math.random()*5)+1
+function playGame() {
+  const randomNumber = Math.floor(Math.random() * 5) + 1;
 
-const userInput = Number(prompt("1~5の数字を入力してください"));
+  const userInput = Number(prompt("1~5の数字を入力してください"));
 
-if(userInput === randomNumber) {
-  alert("当たり！");
-}else {
-  alert("ハズレ！正解は" + rondomNumber + "でした");
-}
+  if (userInput === randomNumber) {
+    alert("当たり！");
+  } else {
+    alert("ハズレ！正解は" + randomNumber + "でした");
+  }
 }
