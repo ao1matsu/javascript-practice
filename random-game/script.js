8c6f7a96a2cd53a19167ed1eb7a9709a90e806f4
ao1matsu <ao1.matsu08@gmail.com>
2026-02-15 21:30:17 +0900

    Create script.js

---
+++ random-game/script.js
@@ -0,0 +1,11 @@
+function PlayGame() {
+const rondomNumber = Math.floor(Math.random()*5)+1
+
+const userInput = Number(prompt("1~5の数字を入力してください"));
+
+if(userInput === randomNuber {
+  alert("当たり！");
+}eles {
+  alert("ハズレ！正解は" + rondomNumber + "でした");
+}
+}
