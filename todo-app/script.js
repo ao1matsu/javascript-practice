8ec67f071f0f84c203526829cbc7963f4be04a1b
ao1matsu <ao1.matsu08@gmail.com>
2026-02-18 21:04:11 +0900

    Update script.js

--- todo-app/script.js
+++ todo-app/script.js
@@ -1,1 +1,17 @@
+const addButton = document.getElementById("addButton");
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
 
+addButton.addEventListener("click",function(){
+    const taskText = taskInput.value;
+
+    if(taskText === ""){
+        return;
+    }
+
+    const li = document.createElement("li");
+    li.textContent = taskText;
+    taskList.append(li);
+
+    taskInput.Value = "";
+});
