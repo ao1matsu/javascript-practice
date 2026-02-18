6b49eb42e4fd5ed73950824017b6539173ac0909
ao1matsu <ao1.matsu08@gmail.com>
2026-02-18 21:51:42 +0900

    Update script.js

--- todo-app/script.js
+++ todo-app/script.js
@@ -2,24 +2,24 @@ const addButton = document.getElementById("addButton");
 const taskInput = document.getElementById("taskInput");
 const taskList = document.getElementById("taskList");
 
-addButton.addEventListener("click",function(){
+addButton.addEventListener("click", function () {
     const taskText = taskInput.value;
 
-    if(taskText === ""){
+    if (taskText === "") {
         return;
     }
 
     const li = document.createElement("li");
     li.textContent = taskText;
 
-const deleteButton = document.createElement("button");
-deleteButton.textContent ="削除";
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "削除";
 
-deleteButton.addEventListener("click",function () {
-    taskList.removeChild(li);
-});
+    deleteButton.addEventListener("click", function () {
+        taskList.removeChild(li);
+    });
 
-
+    li.appendChild(deleteButton);  // ← これ重要
     taskList.appendChild(li);
 
     taskInput.value = "";
