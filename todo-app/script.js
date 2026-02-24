11faa3840215fadf88efb7c0f03c7f171e214ccb
ao1matsu <ao1.matsu08@gmail.com>
2026-02-24 13:34:44 +0900

    Update script.js

--- todo-app/script.js
+++ todo-app/script.js
@@ -14,16 +14,19 @@ addButton.addEventListener("click", function () {
 
     li.addEventListener("click", function () {
     li.classList.toggle("completed");
+        saveTasks();
 });
 
     const deleteButton = document.createElement("button");
     deleteButton.textContent = "削除";
 
-    deleteButton.addEventListener("click", function () {
+    deleteButton.addEventListener("click", function (event) {
+        event.stopPropagation();
         taskList.removeChild(li);
+        saveTask();
     });
 
-    li.appendChild(deleteButton);  // ← これ重要
+    li.appendChild(deleteButton);  
     taskList.appendChild(li);
 
     taskInput.value = "";
@@ -33,5 +36,48 @@ taskInput.addEventListener("keydown", function (event) {
         addButton.click();
     }
 
-    
 });
+//保存する関数
+
+    function saveTakes() {
+    const tasks = [];
+    const liElements = document.querySelectorAll("#taskList li");
+
+    liElements.forEach(function (li){
+        tasks.push({
+            text: li.firstChild.textContent,
+            completed: li.classList.contains("completed")
+        });
+    });
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+        saveTask();
+
+
+        //ページの読み込み時に復元
+         window.addEventListener("load",function(){
+        const savedTasks = JSON.parse(this.localStorage.getItem("tasks")) || [];
+
+        savedTasks.forEach(function (task){
+            const li = document.createElement("li");
+            li.textContent = task.text;
+
+            if(task.completed) {
+                li.classList.toggle("completed");
+                savedTasks();
+        });
+
+        const deleteButton = this.document.createElement("button");
+        deleteButton.textContent = "削除";
+
+        deleteButton.addEventListener("click",function(event){
+            event.stopPropagation();
+            taskList.removeChild(li);
+            savedTasks();
+        });
+
+        li.appendChild(deleteButton);
+        taskList.appendChild(li);
+
+    });
+        
+}
